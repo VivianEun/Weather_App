ee34fa9bec1ac990a7df25b73a5c37132c2ecffd
VivianEun <90486776+VivianEun@users.noreply.github.com>
2021-11-06 17:13:09 +0800

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -40,14 +40,22 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  iconElement.setAttribute(
-    "alt",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
-  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "9789a56f835444e4b7a87bc94ad1c022";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "9789a56f835444e4b7a87bc94ad1c022";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Singapore");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
