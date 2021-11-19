d32f78c4d586c73a8b37003f8109fb66cf49cb8b
VivianEun <90486776+VivianEun@users.noreply.github.com>
2021-11-20 01:21:18 +0800

    Replaced weather forecast HTML with JS function

--- src/app.js
+++ src/app.js
@@ -21,6 +21,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#weather-forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+                  <div class="col-2">
+                    <div class="weather-forecast-date">${day}</div>
+                    <img
+                      src="http://openweathermap.org/img/wn/50d@2x.png"
+                      alt=""
+                      width="36"
+                    />
+                    <div class="weather-forecast-temperature">
+                      <span class="weather-forecast-temperature-max"> 18°</span>
+                      <span class="weather-forecast-temperature-min"> 12°</span>
+                  </div>
+                </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -104,3 +132,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Singapore");
+displayForecast();
